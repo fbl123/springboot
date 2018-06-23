3798b0b3aed7db61b4f468cf76719d53e3f6c804
mac <m15978760154_1@163.com>
2018-06-23 18:28:51 +0800

    f

--- guns/activemq-data/1.sql
+++ guns/activemq-data/1.sql
@@ -1,18 +1,11 @@
-
-create table user_sms
+create table user_call_log
 (
   id int(11) auto_increment,
   user_id int(11) not null comment '用户ID',
-  _id varchar (255) comment '短信序号',
-  thread_id varchar (255) comment '对话的序号，如100，与同一个手机号互发的短信，其序号是相同的',
-  address varchar(255) comment '发件人号码',
-  person varchar(255) comment '发件人',
-  date integer comment '日期',
-  protocol varchar (255) comment '协议0SMS_RPOTO短信，1MMS_PROTO彩信',
-  `read` int(2) comment '是否阅读0未读，1已读',
-  status int(2) comment '短信状态-1接收，0complete,64pending,128failed',
-  type int(2) comment '短信类型1是接收到的，2是已发出',
-  body varchar (512) comment '短信具体内容',
-service_center varchar (255) comment '短信服务中心号码编号',
-primary key(id)
-)comment '用户短信';
+  phone_number varchar(20) comment '通话人号码',
+  call_name varchar(36) comment '通话人备注',
+  duration int(11) comment '通话时长 秒',
+  date DATETIME comment '通话时间 YYYY-MM-DD HH:MM:SS',
+  status varchar(20) comment '状态  打出out 收入in',
+  primary key(id)
+)comment '用户通话记录';
--- guns/src/main/java/设计模式/工厂模式/Test.java
+++ guns/src/main/java/设计模式/工厂模式/Test.java
@@ -1,12 +1,16 @@
 package 设计模式.工厂模式;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
 
 
     public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
-        Shape shape = shapeFactory.createShape("1");
-        shape.draw();
+//        ShapeFactory shapeF
+        Date date=new Date(1498391898962L);
+        System.out.println(date);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date));
     }
 
 }
