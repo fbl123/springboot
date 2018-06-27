8a1587d36474649f0ba6016dfee5a8142a956cdd
mac <m15978760154_1@163.com>
2018-06-27 09:55:46 +0800

    f

--- guns/activemq-data/1.sql
+++ guns/activemq-data/1.sql
@@ -1,11 +1,34 @@
-create table user_call_log
-(
-  id int(11) auto_increment,
-  user_id int(11) not null comment '用户ID',
-  call_number varchar(20) comment '通话人号码',
-  call_name varchar(36) comment '通话人备注',
-  duration int(11) comment '通话时长 秒',
-  date DATETIME comment '通话时间 YYYY-MM-DD HH:MM:SS',
-  status varchar(20) comment '状态  1,打出out 2,收入in 3未接来电',
-  primary key(id)
-)comment '用户通话记录';
+insert ignore into user_sms
+(`user_id`,
+serial_number,
+thread_id,
+address,
+person,
+date,
+protocol,
+read,
+status,
+type,
+body,
+service_center
+)
+values
+<foreach collection="list" item="sms" separator=",">
+  (
+  #{sms.userId},
+  #{sms.serialNumber},
+  #{sms.threadId},
+  #{sms.address},
+  #{sms.person},
+  #{sms.date},
+  #{sms.protocol},
+  #{sms.read},
+  #{sms.status},
+  #{sms.type},
+  #{sms.body},
+  #{sms.serviceCenter}
+  )
+</foreach>
+
+alter ignore table user add unique index(user_id, address,date);
+
