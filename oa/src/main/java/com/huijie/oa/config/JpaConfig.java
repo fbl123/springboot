0c0bf402f11439f432f437bc9114f2db19a935cd
fbl <m15978760154_1@163.com>
2019-03-10 10:05:46 +0800

    '1'

--- oa/src/main/java/com/huijie/oa/config/JpaConfig.java
+++
@@ -1,8 +0,0 @@
-package com.huijie.oa.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JpaConfig {
-}
--- springsecurity/src/main/java/com/huijie/mapper/UserMapper.java
+++
@@ -1,4 +0,0 @@
-package com.huijie.mapper;
-
-public interface UserMapper {
-}
